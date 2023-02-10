3dca4238ff0754c9cc7ed5bad397692ced31d3e8
DevEless <qdelameilleure@gmail.com>
2023-02-10 10:19:39 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from './components/Buttons'
 import TresDouList from './components/TresDouList';
 import Convertisseur from './components/Convertisseur';
 import SignUpForm from './components/Formulaire';
+import Enigme from './components/enigme';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,8 @@ function App() {
     <TresDouList/>
     <Convertisseur />
     <SignUpForm/>
+    <Enigme/>
+
     </div>
   );
 }
